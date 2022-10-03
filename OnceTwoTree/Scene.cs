949a3b8212b48a3a480b030d4bb98e1534375b00
WIROONYAPAT NAOSAWAT <wiroonyapat_n@cmu.ac.th>
2022-10-03 20:47:38 +0700

    เพิ่มต่อ

--- OnceTwoTree/Scene.cs
+++ OnceTwoTree/Scene.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace OnceTwoTree
 {
     public class Scene
     {
+        protected EventHandler ScreenEvent;
 
+        public Scene(EventHandler theScreenEvent)
+        {
+            ScreenEvent = theScreenEvent;
+        }
+
+        public virtual void Update(GameTime theTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch theBatch)
+        {
+
+        }
     }
 }
---
+++ OnceTwoTree/TitleScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OnceTwoTree
+{
+    public class TitleScreen : Scene
+    {
+        Texture2D menuTexture;
+        Game1 game;
+        public TitleScreen(Game1 game, EventHandler theScreenEvent) : base(theScreenEvent)
+        {
+            //menuTexture = game.Content.Load<Texture2D>("title");
+            this.game = game;
+        }
+        public override void Update(GameTime theTime)
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.A) == true)
+            {
+                
+                return;
+            }
+
+            base.Update(theTime);
+        }
+
+        public override void Draw(SpriteBatch theBatch)
+        {
+            theBatch.Draw(menuTexture, Vector2.Zero, Color.White); base.Draw(theBatch);
+        }
+    }
+}
