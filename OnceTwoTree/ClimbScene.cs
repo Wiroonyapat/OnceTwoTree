8d9c7c4e38f9df83250397a8f7afb3b2bab992a5
chawanvit <chawanvit_mai@cmu.ac.th>
2022-10-04 00:17:04 +0700

    Update Game Feedback

--- OnceTwoTree/ClimbScene.cs
+++ OnceTwoTree/ClimbScene.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace OnceTwoTree
 {
@@ -16,7 +17,8 @@ namespace OnceTwoTree
         Vector2 playerPos,playerScale, triggerPos;
         Vector2 barPos,dropPos,skillCheckPosL,skillCheckPosR;
         Vector2 dropScale,barScale,triggerScale,scScaleL,scScaleR;
-
+        List<Color> colorFeedBack = new List<Color>();
+        int playerColor,scLColor,scRColor;
         public bool leftHand;
         public bool PanelCheck = true;
         KeyboardState ks1,oldKsP1;
@@ -33,6 +35,13 @@ namespace OnceTwoTree
             block = game.Content.Load<Texture2D>("Character\\block");
             towerTexture = game.Content.Load<Texture2D>("Maptile\\Normal-wall-1");
             font = game.Content.Load<SpriteFont>("Font");
+
+            #region Add Color //0.White 1.Red 2.Green
+            colorFeedBack.Add(Color.White);
+            colorFeedBack.Add(Color.Red);
+            colorFeedBack.Add(Color.LightGreen);
+
+            #endregion
             GameConfig();
         }
 
@@ -70,14 +79,15 @@ namespace OnceTwoTree
                 theBatch.Draw(towerTexture, new Vector2(game.Window.ClientBounds.Width - 140, 96 * i), null, Color.White, 0, Vector2.Zero, 2, SpriteEffects.None, 0); base.Draw(theBatch);
             }
             
-            theBatch.Draw(character, playerPos, new Rectangle(0, 0, 24, 48), Color.White, 0, Vector2.Zero, playerScale, SpriteEffects.None, 0); base.Draw(theBatch);
+            theBatch.Draw(character, playerPos, new Rectangle(0, 0, 24, 48), colorFeedBack[playerColor], 0, Vector2.Zero, playerScale, SpriteEffects.None, 0); base.Draw(theBatch);
             //Whitebar
             theBatch.Draw(block, barPos, null, Color.White, 0, Vector2.Zero,barScale, SpriteEffects.None, 0); base.Draw(theBatch);
             //Drop Check
             theBatch.Draw(block, dropPos, null, Color.Yellow, 0, Vector2.Zero, dropScale, SpriteEffects.None, 0); base.Draw(theBatch);
-            //Skillcheck
-            theBatch.Draw(block, skillCheckPosL, null, Color.Red, 0, Vector2.Zero, scScaleL, SpriteEffects.None, 0); base.Draw(theBatch);
-            theBatch.Draw(block, skillCheckPosR, null, Color.Red, 0, Vector2.Zero, scScaleR, SpriteEffects.None, 0); base.Draw(theBatch);
+            //SkillcheckL
+            theBatch.Draw(block, skillCheckPosL, null, colorFeedBack[scLColor], 0, Vector2.Zero, scScaleL, SpriteEffects.None, 0); base.Draw(theBatch);
+            //SkillcheckR
+            theBatch.Draw(block, skillCheckPosR, null, colorFeedBack[scRColor], 0, Vector2.Zero, scScaleR, SpriteEffects.None, 0); base.Draw(theBatch);
             //Trigger
             theBatch.Draw(block, triggerPos, null, Color.Black, 0, Vector2.Zero, triggerScale, SpriteEffects.None, 0); base.Draw(theBatch);
 
@@ -124,7 +134,11 @@ namespace OnceTwoTree
             triggerScale = new Vector2(1, 1);
             triggerPos = new Vector2(barPos.X + (block.Width * barScale.X / 2 - (block.Width / 2)), this.game.Window.ClientBounds.Height - 80);
             #endregion
-
+            #region Color Feedback
+            playerColor = 0;
+            scLColor = 1;
+            scRColor = 2;
+            #endregion
             onPressedL = false;
             onPressedR = false;
             onPressed = false;
@@ -141,6 +155,16 @@ namespace OnceTwoTree
                     trigerSpeed *= -1;
                 }
 
+            }
+
+            if (leftHand)
+            {
+                scLColor = 2;
+                scRColor = 1;
+            }else if (!leftHand)
+            {
+                scLColor = 1;
+                scRColor = 2;
             }
 
         }
@@ -175,13 +199,15 @@ namespace OnceTwoTree
                 //Skill for check left hand
                 if (triggerRec.Intersects(scLRec) && leftHand && energy > 0)
                 {
-                        leftHand = false;
-                        energy -= 2;
-                        playerPos.Y -= characterSpeed;
+                    playerColor = 2;
+                    leftHand = false;
+                    energy -= 2;
+                    playerPos.Y -= characterSpeed;
                 }
                 //Check On Drop Block
                 if (triggerRec.Intersects(dropRec))
                 {
+                    playerColor = 1;
                     energy -= 2;
                     playerPos.Y += 10;
                 }
@@ -189,6 +215,7 @@ namespace OnceTwoTree
             }
             else if (ks1.IsKeyUp(Keys.A) && onPressedL)
             {
+                playerColor = 0;
                 onPressedL = false;
             }
 
@@ -198,6 +225,7 @@ namespace OnceTwoTree
                 //Skill for check Right hand
                 if (triggerRec.Intersects(scRRec)&& !leftHand && energy > 0)
                 {
+                    playerColor = 2;
                     leftHand = true;
                     energy -= 2;
                     playerPos.Y -= characterSpeed;
@@ -205,6 +233,7 @@ namespace OnceTwoTree
                 //Check On Drop Block
                 if (triggerRec.Intersects(dropRec))
                 {
+                    playerColor = 1;
                     energy -= 2;
                     playerPos.Y += characterSpeed;
                 }
@@ -212,6 +241,7 @@ namespace OnceTwoTree
             }
             else if (ks1.IsKeyUp(Keys.D) && onPressedR)
             {
+                playerColor = 0;
                 onPressedR = false;
             }
 
